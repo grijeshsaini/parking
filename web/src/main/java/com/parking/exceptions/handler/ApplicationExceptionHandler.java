81513bf77b222159e71813089330cc8e30d4d79d
Grijesh Saini <grijesh.saini@digital.hmrc.gov.uk>
2017-10-07 21:21:01 +0100

    Rename file

--- web/src/main/java/com/parking/exceptions/handler/ApplicationExceptionHandler.java
+++ web/src/main/java/com/parking/exceptions/handler/ApplicationExceptionHandler.java
@@ -3,14 +3,15 @@ package com.parking.exceptions.handler;
 import com.parking.exceptions.DataNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class ExceptionHandler extends ResponseEntityExceptionHandler {
+public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Data not found")
-    @org.springframework.web.bind.annotation.ExceptionHandler(value = {DataNotFoundException.class})
+    @ExceptionHandler(value = {DataNotFoundException.class})
     protected void handleDataNotFound() {
     }
 }
