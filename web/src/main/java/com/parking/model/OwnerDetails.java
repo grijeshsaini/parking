246d8b6f943c5af31f8bcd2a2104740164e08120
Grijesh Saini <grijesh.saini@digital.hmrc.gov.uk>
2017-10-07 21:00:59 +0100

    Created Global Exception Handler

--- web/src/main/java/com/parking/model/OwnerDetails.java
+++ web/src/main/java/com/parking/model/OwnerDetails.java
@@ -3,7 +3,7 @@ package com.parking.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document
+@Document(collection = "ownerdetails")
 public class OwnerDetails {
     @Id
     private String id;
