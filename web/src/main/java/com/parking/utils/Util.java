9c681bf24875aa11c322c8049f0c0c6724307790
grijeshsaini <grijesh.rajoria@gmail.com>
2017-11-09 11:24:44 +0000

    Fixed Bug

--- web/src/main/java/com/parking/utils/Util.java
+++ web/src/main/java/com/parking/utils/Util.java
@@ -11,7 +11,7 @@ import com.parking.model.Vehicle;
 public class Util {
 
 	public static ParkingDetails buildParkingDetails(Parking details) {
-		return new ParkingDetails.Builder().name(details.getName()).building(details.getBuilding())
+		return new ParkingDetails.Builder().id(details.getId()).name(details.getName()).building(details.getBuilding())
 				.emailAddress(details.getEmailAddress()).floor(details.getFloor())
 				.mobileNumber(details.getMobileNumber()).seat(details.getSeat()).workNumber(details.getWorkNumber())
 				.vehicles(Util.buildVehicleDetails(details.getVehicles())).build();
